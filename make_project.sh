3dae2ab5f6c7d5d3c8c629cf0c7c5c117a600658
Spade31 <spadesuper2@gmail.com>
2026-06-30 18:29:27 +0530

    completed shell scripting

---
+++ make_project.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Creating a new project setup"
+mkdir -p my_new_project/src
+mkdir -p my_new_project/docs
+touch my_new_project/src/main.js
+touch my_new_project/README.md
+echo "Done run ls my_new_project "
